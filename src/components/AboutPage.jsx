0d7800fd625f6a3f631c49a1be404139674b869f
John Jacob <jacobjohnrobert@gmail.com>
2017-09-28 20:54:29 -0500

    Fix typo

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -11,7 +11,7 @@ export default class AboutPage extends Component {
     return (
       <div className="about-page">
         <article>
-          John Jacob has been building software professionaly since 2016, specializing in web technologies. He studied Liberal Arts at Bard College at Simon's Rock, and Chinese Language and Literature at University of Maryland, College Park. In 2016, John built the demo and beta versions of BalanceCXI's flagship product, Zacoustic. John lives in Austin, Texas.
+          John Jacob has been building software professionally since 2016, specializing in web technologies. He studied Liberal Arts at Bard College at Simon's Rock, and Chinese Language and Literature at University of Maryland, College Park. In 2016, John built the demo and beta versions of BalanceCXI's flagship product, Zacoustic. John lives in Austin, Texas.
         </article>
         <BackButton />
       </div>
